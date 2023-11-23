02bf46b3598e89acdd66ee1a0c80b1c869a6704f
Juan Camilo Rivas molina <bogota.diamond@hotmail.com>
2023-11-23 11:14:49 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import datosDeEnvios from "./database"; // Asegúrate de tener la ruta correcta al archivo database.js
+import "./App.css";
+import {
+  faWarehouse,
+  faBars,
+  faLocationDot,
+  faBoxOpen,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-function App() {
+const Pantalla = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="pantalla">
+      <header className="header-black">
+        <h1>Bodega</h1>
       </header>
+      <div className="listaEnvios">
+        {datosDeEnvios.map((envio, index) => (
+          <div key={index} className="envio">
+            <div className="infoLeft">
+              <p>
+                <strong>{envio.numeroGuia}</strong>{" "}
+              </p>
+              <p>{envio.fechaIngreso}</p>
+              <p>{envio.descripcion}</p>
+            </div>
+            <p className="peso">
+              {" "}
+              <strong>Peso: {envio.peso}</strong>
+            </p>
+          </div>
+        ))}
+      </div>
+      <div className="footer">
+        <FontAwesomeIcon icon={faWarehouse} size="lg" color="gray" />
+        <FontAwesomeIcon icon={faBoxOpen} size="lg" color="gray" />
+        <FontAwesomeIcon icon={faLocationDot} size="lg" color="gray" />
+        <FontAwesomeIcon icon={faBars} size="lg" color="gray" />
+      </div>
     </div>
   );
-}
+};
 
-export default App;
+export default Pantalla;
---
+++ src/database.js
@@ -0,0 +1,30 @@
+
+const datosDeEnvios = [
+    {
+      peso: 6,
+      numeroGuia: '2121654942',
+      fechaIngreso: '2 de Septiembre, 2:02pm.',
+      descripcion: 'COMPUTADOR/TABLET',
+    },
+    {
+        peso: 1,
+        numeroGuia: '2121654942',
+        fechaIngreso: '2 de Septiembre, 2:02pm.',
+        descripcion: 'CELULAR',
+      },
+      {
+        peso: 36,
+        numeroGuia: '2121654936',
+        fechaIngreso: '2 de Septiembre, 2:02pm.',
+        descripcion: 'CORREO REGULAR/ART. HOGAR',
+      },
+      {
+        peso: 5,
+        numeroGuia: '2121654987',
+        fechaIngreso: '2 de Septiembre, 2:02pm.',
+        descripcion: 'CORREO REGULAR/ART. HOGAR',
+      },
+    ];
+  
+  export default datosDeEnvios;
+  
